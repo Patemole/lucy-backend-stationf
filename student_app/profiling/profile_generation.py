18ce9c788b95f0444ba6b5f9f172eb481ca43e97
mathieuperez13 <mathieuperez13@gmail.com>
2024-08-06 12:11:37 +0200

    student profile without groq and in chithcat

--- student_app/profiling/profile_generation.py
+++ student_app/profiling/profile_generation.py
@@ -15,6 +15,22 @@ os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 MODEL_NAME = "llama3-70b-8192"
 
+
+# Fonction principale renommée
+def LLM_profile_generation(username: str, academic_advisor: str, year: str, university: str, faculty: str, major: str, minor: str):
+
+    print("\n")
+    print("\n")
+    print(f"Profiling of the user")
+    print(f"username: {username}, year: {year}, university: {university}, School: {faculty}, major: {major}, minor: {minor}, academic_advisor: {academic_advisor}")
+
+    try:
+        student_profile = f"My name is {username}. I am enrolled at {university} in the {faculty} school. I am majoring in {major} and minoring in {minor}. Currently in my {year} year, I am guided by academic advisor: {academic_advisor}."
+        return student_profile
+    except Exception as e:
+        print(e)
+
+"""
 # Fonction principale renommée
 def LLM_profile_generation(prompt_answering: str, username: str, academic_advisor: str, year: str, university: str, faculty: str, major: str, minor: str):
 
@@ -43,3 +59,4 @@ def LLM_profile_generation(prompt_answering: str, username: str, academic_adviso
         return response.content
     except Exception as e:
         print(e)
+"""
