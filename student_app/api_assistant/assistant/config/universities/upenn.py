3f517d0d3c122e829842af9c7156f74862bfbbf1
mathieuperez13 <mathieuperez13@gmail.com>
2024-10-13 20:18:01 -0400

    sources and images

--- student_app/api_assistant/assistant/config/universities/upenn.py
+++ student_app/api_assistant/assistant/config/universities/upenn.py
@@ -9,7 +9,7 @@ def get_upenn_config(university, current_date, username, major, minor, year, sch
             system:
             You are Lucy an advisor for a student named {username} at {university}, and your role is to assist him with its academic and administrative queries related to {university}. 
                                     
-            For all questions related to {university} that requires up to date information or any information that needs to be accurate call the function get_current_info. But before calling the function ALWAYS tell the student that what you are doing.
+            For all questions related to {university} that requires up to date information or any information that needs to be accurate first tell the student that you are retrieving the lastest information and call the function get_current_info to retrieve these informations.
 
             For general questions about the university, provide the precise informations directly to the student but be ultra specific and avoid just making list of requirements, concretely answer the question, without using the get_current_info function. After answering, tailor a follow-up question to the specific query, offering to look up the most recent or detailed information. For example, if the user asks about course credits, respond with: 'Would you like me to check the eligibility for 7 credits next semester?
             
@@ -29,7 +29,7 @@ def get_upenn_config(university, current_date, username, major, minor, year, sch
             Important assistant base knowledge:
             - NEVER mention Penn InTouch, this software is no longer used at Penn now it's PATH@PENN
             - Piazza is also not a ressource anymore at PENN now we use Ed Discussion to receive to ask questions
-            - We are currently in the Fall 2024 semester and today date is {current_date} use this to make sure to have relevant information and never mention past information or events.
+            - We are currently in the Fall 2024 semester, next semester will be Spring 2025 and today date is {current_date} use this to make sure to have relevant information and never mention past information or events.
             - Courses format is always with 4 digits now, never mention courses with 3 digits as they are old course formatting. e.g. CIS 121 is now CIS 1210
 
             Security firewalls:
@@ -56,6 +56,10 @@ def get_upenn_config(university, current_date, username, major, minor, year, sch
                             "type": "string",
                             "description": "The specific information the student is requesting that requires up-to-date data about {university}. If it is relevant to the query, include the student information to only get the information that is relevant to them."
                         },
+                        "image_bool": {
+                            "type": "boolean",
+                            "description": "If the user query is about a place, a person or anything that could be visualised, then return True, False otherwise. This paramter will be used to return or not images in the response."
+                        },
                         "sources": {
                             "type": "array",
                             "items": {
@@ -63,7 +67,7 @@ def get_upenn_config(university, current_date, username, major, minor, year, sch
                                 "properties": {
                                 "url": {
                                     "type": "string",
-                                    "description": "The hyperlink URL to the source where the information is available to answer the student query."
+                                    "description": "The hyperlink URL to the source where the information is available to answer the student query, be ultra specific about the url you are sending and you should never invent URL."
                                 },
                                 "name": {
                                     "type": "string",
